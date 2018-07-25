dcbf590d0182b3b82f683575ee9f2963230f9f28
jianggc <jianggaocheng@hotmail.com>
2018-07-25 23:18:49 +0800

    测试第一版，完成5.5 英寸设备适配

--- index.js
+++ index.js
@@ -1,14 +1,84 @@
 const wda = require("wda");
 const wdaDriver = require('wda-driver');
+const Jimp = require("jimp");
+const async = require("async");
 
-const wdaServerURL = 'http://192.168.2.2:8100';
+/**
+ * 参数配置部分
+ */
 
-const c = new wdaDriver.Client(wdaServerURL);
 
-async function play(){
-  let s = await c.session();
-  console.log('c',s);
+// 手机IP地址
+const wdaServerURL = 'http://192.168.123.56:8100';
+
+// iphone 6sp/7p/8p
+const positon = {
+  menuPos: {
+    x: 360,
+    y: 30,
+  },
+
+  shareButton: {
+    x: 42,
+    y: 481,
+  },
+
+  fileHelper: {
+    x: 170,
+    y: 226
+  },
+
+  confirmShare: {
+    x: 286,
+    y: 480
+  },
+}
+
+// 按钮点击间隔
+const SLEEP = 800;
+
+
+/**
+ * 循环点击各个按钮，进行分享
+ */
+
+const client = new wdaDriver.Client(wdaServerURL);
+
+async function play() {
+  let session = await client.session();
+
+  // 点击菜单按钮
+  await session.tap(positon.menuPos.x, positon.menuPos.y);
+
+  // 暂停等待UI
+  await sleep(SLEEP);
+
+  // 点击分享按钮
+  await session.tap(positon.shareButton.x, positon.shareButton.y);
+
+  // 暂停等待UI
+  await sleep(SLEEP);
+
+  // 选择分享联系人第一位（文件传输助手）
+  await session.tap(positon.fileHelper.x, positon.fileHelper.y);
+
+  // 暂停等待UI
+  await sleep(SLEEP);
+
+  // 点击确定分享
+  await session.tap(positon.confirmShare.x, positon.confirmShare.y);
+
+  // 暂停等待UI
+  await sleep(SLEEP);
 };
 
-// connect();
-play();
+const sleep = async function (ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+
+async.forever(play);
+// play();
